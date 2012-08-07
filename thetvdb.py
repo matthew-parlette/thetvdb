ccc84eef238f5730c350dfe9c8486ddd8b5e75b0
Matt Parlette <matthew.parlette@gmail.com>
2012-08-07 11:53:47 -0400

    Changed indent to 4 spaces to comply with PEP8

--- thetvdb.py
+++ thetvdb.py
@@ -1,3 +1,6 @@
 class TheTVDB:
-  def __init__(self,seriesid = None):
-    self.api_key = '2CDD930540D49661'
+    def __init__(self,series_id = None):
+        self.api_key = '2CDD930540D49661'
+        self.series_id = series_id
+        if self.series_id is not None:
+            pass
