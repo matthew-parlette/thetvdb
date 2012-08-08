9ac582f7442ad8a80fc0c9bdf23d72e6bda0cb5a
Matt Parlette <matthew.parlette@gmail.com>
2012-08-08 15:30:38 -0400

    automated tests for thetvdb.py

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from thetvdb import TVShow
+
+print "Testing thetvdb.py"
+print "------------------\n"
+print "Loading Community..."
+tv = TVShow('94571')
+print ""
+print "Printing first episode filename..."
+print tv.get_filename(1,1)
+print ""
+print "Printing filename for fourth episode of season 2 with one flag..."
+print tv.get_filename(2,4,['HDTV'])
+print ""
+print "Printing filename for seventh episode of season 3 with two flag..."
+print tv.get_filename(3,7,['HDTV','x264'])
+print ""
+print "Searching Voyager..."
+print tv.search("voyager")
+print ""
